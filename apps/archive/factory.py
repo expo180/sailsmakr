1bd71eb0443a701b401a0347f9d973f2057b7463
expo180 <badersaadousalissou@gmail.com>
2024-12-26 11:17:43 +0100

    factory.py updated

--- apps/archive/factory.py
+++ apps/archive/factory.py
@@ -2,17 +2,13 @@ from flask import Flask
 from flask_cors import CORS 
 from config import config
 from .. import db
-from ..models.general.folder import Folder
-
 
 def create_app(config_name='development'):
-    app = Flask(__name__, static_folder='static')
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
-    db.init_app(app)
-    CORS(app, resources={r"/archive/v1/*": {"origins": "*"}})
-
+    cloudsquish = Flask(__name__, static_folder='static')
+    cloudsquish.config.from_object(config['development'])
+    config['development'].init_app(cloudsquish)
+    db.init_app(cloudsquish)
     from . import archive as archive_blueprint
-    app.register_blueprint(archive_blueprint, url_prefix='/archive/v1')
-
-    return app
+    cloudsquish.register_blueprint(archive_blueprint, url_prefix='/archive/v1')
+    CORS(cloudsquish)
+    return cloudsquish
