920c0e3b12d40a5f6b6d0787c83ec8ecf5828204
expo180 <badersaadousalissou@gmail.com>
2025-01-02 10:50:40 +0100

    database path problem fixed for prod environmenet on localhost

--- config.py
+++ config.py
@@ -3,9 +3,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def get_database_path(database_name="app.db"):
+    """
+    Generate a writable path for the database file based on the OS.
+    The database will be stored in the 'Documents/Gusa folder inside the user's home directory.
+    """
+    home_dir = os.path.expanduser("~")
+
+    documents_dir = os.path.join(home_dir, "Documents", "Gusa")
+
+    os.makedirs(documents_dir, exist_ok=True)
+
+    return os.path.join(documents_dir, database_name)
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SECURITY_PASSWORD_SALT=os.environ.get('SECURITY_PASSWORD_SALT')
+    SECURITY_PASSWORD_SALT = os.environ.get('SECURITY_PASSWORD_SALT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
@@ -25,19 +39,23 @@ class Config:
     def init_app(app):
         pass
 
+
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{get_database_path('app.db')}"
+
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{get_database_path('app.db')}"
+
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('PRODUCTION_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{get_database_path('app.db')}"
     PLACEHOLDER_STATIC_URL = os.environ.get('PLACEHOLDER_PRODUCTION_STATIC_URL')
     LOGIN_URL = os.environ.get('PRODUCTION_LOGIN_URL')
     SERVER_URL = os.environ.get('PRODUCTION_SERVER_URL')
+
 
 config = {
     'development': DevelopmentConfig,
